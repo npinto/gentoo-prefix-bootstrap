c85ab395effc39930bdb3f6e912b42810168935c
Nicolas Pinto <pinto@mit.edu>
2011-10-22 14:34:37 -0400

    Add helpers.mk and debug action

---
+++ helpers.mk
@@ -0,0 +1,50 @@
+ifndef HELPERS_MK
+HELPERS_MK=helpers.mk
+
+# ============================================================================
+# -- Helpers
+# ============================================================================
+
+debug:
+	export EPREFIX=${EPREFIX}-debug
+
+clean:
+	rm -f bootstrap-prefix-patched.sh
+	rm -f bootstrap-prefix-*.patch
+	rm -f install/stage*
+
+uninstall: uninstall-ask uninstall-force
+
+uninstall-ask:
+	@echo "*************************************************************"
+	@echo "** WARNING WARNING WARNING WARNING WARNING WARNING WARNING **"
+	@echo "*************************************************************"
+	@echo "Are you sure you want to uninstall ${EPREFIX} ??"
+	@echo "Press any key to continue or CTRL-C to cancel."
+	@echo
+	@read null
+	@echo "*************************************************************"
+	@echo "Are you really really sure ???"
+	@echo "Press any key to continue or CTRL-C to cancel."
+	@echo
+	@read null
+
+uninstall-force: clean
+	mv -f ${EPREFIX} ${EPREFIX}.deleteme 2> /dev/null || exit 0
+	rm -rf ${EPREFIX}.deleteme
+
+backup: ${EPREFIX}
+	mv -vf ${EPREFIX} ${EPREFIX}-backup-$(shell date +"%Y-%m-%d_%Hh%Mm%Ss")
+
+help:
+	@cat README.txt
+	@echo
+	@echo "Available actions (targets):"
+	@echo "----------------------------"
+	@./utils/list_make_targets.sh
+	@echo
+	@echo "To see which commands will be executed by each action, use:"
+	@echo "\$$ make -n action"
+	@echo
+
+endif
--- system.mk
+++ system.mk
@@ -183,46 +183,4 @@ install/stage4-workarounds: install/stage3 install/stage4-config
 	emerge openssh
 	touch $@
 
-# ============================================================================
-# -- Helpers
-# ============================================================================
-clean:
-	rm -f bootstrap-prefix-patched.sh
-	rm -f bootstrap-prefix-*.patch
-	rm -f install/stage*
-
-uninstall: uninstall-ask uninstall-force
-
-uninstall-ask:
-	@echo "*************************************************************"
-	@echo "** WARNING WARNING WARNING WARNING WARNING WARNING WARNING **"
-	@echo "*************************************************************"
-	@echo "Are you sure you want to uninstall ${EPREFIX} ??"
-	@echo "Press any key to continue or CTRL-C to cancel."
-	@echo
-	@read null
-	@echo "*************************************************************"
-	@echo "Are you really really sure ???"
-	@echo "Press any key to continue or CTRL-C to cancel."
-	@echo
-	@read null
-
-uninstall-force: clean
-	mv -f ${EPREFIX} ${EPREFIX}.deleteme 2> /dev/null || exit 0
-	rm -rf ${EPREFIX}.deleteme
-
-backup: ${EPREFIX}
-	mv -vf ${EPREFIX} ${EPREFIX}-backup-$(shell date +"%Y-%m-%d_%Hh%Mm%Ss")
-
-help:
-	@cat README.txt
-	@echo
-	@echo "Available actions (targets):"
-	@echo "----------------------------"
-	@./utils/list_make_targets.sh
-	@echo
-	@echo "To see which commands will be executed by each action, use:"
-	@echo "\$$ make -n action"
-	@echo
-
 endif
