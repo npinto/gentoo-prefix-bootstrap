4bec38f3a822b9347f14d37fb0be3aac5eae2b73
Nicolas Pinto <pinto@mit.edu>
2011-10-27 16:46:48 -0400

    FIX

--- history.working.sh
+++ history.working.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -x
+set -e
+
 source ./update_env.sh
+. ./update_env.sh
 
 make uninstall-force
 make install/stage1
 make install/stage2-up-to-bison
 
 # make -n install/stage2
-emerge --nodeps tar
+#emerge --nodeps tar
 
 #ACCEPT_KEYWORDS="**" emerge --nodeps "=app-arch/tar-1.23*"
 cp -vf files/local_overlay/tar/tar-1.23-r4.ebuild ${EPREFIX}/usr/portage/app-arch/tar/
