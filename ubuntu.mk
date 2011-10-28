36c840d74eb7ec6389e86b5e7c74618389b12d86
Nicolas Pinto <pinto@mit.edu>
2011-10-28 03:56:54 -0400

    na

--- ubuntu.mk
+++ ubuntu.mk
@@ -1,9 +1,8 @@
+
 include Makefile
 
 default: uninstall-force install/stage1 install/stage2-up-to-bison
-    emerge --nodeps --oneshot binutils
-    emerge --nodeps --oneshot gcc-config
-    emerge --nodeps app-arch/tar
-    LIBRARY_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/lib/ -name "libc.so" | tail -n1))))/../ INCLUDE_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/include/ -name 'errno.h' | grep -e '/asm/errno.h' | tail -n1))))/../ emerge --oneshot --nodeps "=gcc-4.2*"
-
-
+	emerge --nodeps --oneshot binutils
+	emerge --nodeps --oneshot gcc-config
+	emerge --nodeps app-arch/tar
+	LIBRARY_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/lib/ -name "libc.so" | tail -n1))))/../ INCLUDE_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/include/ -name 'errno.h' | grep -e '/asm/errno.h' | tail -n1))))/../ emerge --oneshot --nodeps "=gcc-4.2*"
