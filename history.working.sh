a07ffa45214e72f80949537c7abe76685fcd1261
Nicolas Pinto <pinto@mit.edu>
2011-10-28 01:24:30 -0400

    na

--- history.working.sh
+++ history.working.sh
@@ -18,13 +18,14 @@ make install/stage2-up-to-bison
 #emerge --nodeps tar
 
 #ACCEPT_KEYWORDS="**" emerge --nodeps "=app-arch/tar-1.23*"
-mkdir -p ${EPREFIX}/usr/portage/app-arch/tar
-cp -vf files/local_overlay/app-arch/tar/tar-1.23-r4.ebuild ${EPREFIX}/usr/portage/app-arch/tar/
-ebuild ${EPREFIX}/usr/portage/app-arch/tar/tar-1.23-r4.ebuild manifest
-emerge --nodeps "=app-arch/tar-1.23*"
+#mkdir -p ${EPREFIX}/usr/portage/app-arch/tar
+#cp -vf files/local_overlay/app-arch/tar/tar-1.23-r4.ebuild ${EPREFIX}/usr/portage/app-arch/tar/
+#ebuild ${EPREFIX}/usr/portage/app-arch/tar/tar-1.23-r4.ebuild manifest
+#emerge --nodeps "=app-arch/tar-1.23*"
 
 #mkdir -p ${EPREFIX}/usr/portage/app-arch/tar files/local_overlay/
 #cp -a ${EPREFIX}/usr/portage/app-arch/tar files/local_overlay/
 
+emerge --nodeps tar
 LIBRARY_PATH=/usr/lib/x86_64-linux-gnu emerge --nodeps --oneshot binutils gcc-config '=gcc-4.2*'
 
