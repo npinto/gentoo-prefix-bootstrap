99b559599a37409fc494b39b7b2ab2939fa21c6c
Nicolas Pinto <pinto@mit.edu>
2011-10-28 03:25:45 -0400

    na

--- history.working.sh
+++ history.working.sh
@@ -31,5 +31,7 @@ make install/stage2-up-to-bison
 emerge --nodeps --oneshot binutils
 emerge --nodeps --oneshot gcc-config
 emerge --nodeps app-arch/tar
-LIBRARY_PATH=/usr/lib/x86_64-linux-gnu emerge --nodeps --oneshot '=gcc-4.2*'
+#LIBRARY_PATH=/usr/lib/x86_64-linux-gnu emerge --nodeps --oneshot '=gcc-4.2*'
+LIBRARY_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/lib/ -name "libc.so" | tail -n1))))/../ INCLUDE_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/include/ -name 'errno.h' | grep -e '/asm/errno.h' | tail -n1))))/../ emerge --oneshot --nodeps "=gcc-4.2*"
+
 
