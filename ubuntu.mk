8341ec59c52c92202054fbd47aee44fb328390ec
Nicolas Pinto <pinto@mit.edu>
2011-10-28 21:39:16 -0400

    na

--- ubuntu.mk
+++ ubuntu.mk
@@ -4,8 +4,16 @@ all: default
 include init.mk
 include system.mk
 
-default: uninstall-force install/stage1 install/stage2-up-to-bison
+default: uninstall-force install/stage1 install/stage2-up-to-bison gcc-ubuntu install/stage2-portage  install/stage3 install/stage4
+
+gcc-ubuntu:
 	emerge --nodeps --oneshot binutils
 	emerge --nodeps --oneshot gcc-config
 	emerge --nodeps app-arch/tar
 	LIBRARY_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/lib/ -name "libc.so" | tail -n1)))) INCLUDE_PATH=$(shell echo $(shell dirname $(shell echo $(shell find /usr/include/ -name 'errno.h' | grep -e '/asm/errno.h' | tail -n1))))/../ emerge --oneshot --nodeps "=gcc-4.2*"
+	touch install/stage2-binutils
+	touch install/stage2-gcc
+	touch $@
+
+
+
